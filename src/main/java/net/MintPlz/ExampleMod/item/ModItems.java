4aff468f43204c6c604f7cd0d8ccff4fedebbe23
MintPlz <73250650+MintPlz@users.noreply.github.com>
2023-03-06 15:46:44 -0600

    Changed attributes for the black opal pickaxe. Added the black opal tier.
    Will probably have to go back and fix the tier so it's not just stuck in the mod items class. Also, I have not figured out the tier sorting registry.

--- src/main/java/net/MintPlz/ExampleMod/item/ModItems.java
+++ src/main/java/net/MintPlz/ExampleMod/item/ModItems.java
@@ -1,9 +1,10 @@
 package net.MintPlz.ExampleMod.item;
 
 import net.MintPlz.ExampleMod.ExampleMod;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.PickaxeItem;
-import net.minecraft.world.item.Tiers;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeTier;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -13,6 +14,11 @@ public class ModItems {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ExampleMod.MODID);
 
+    public static final Tier BLACKOPAL = new ForgeTier(4, 5000, 200f, 4f, 22,
+            null,
+            () -> {return Ingredient.of(Items.NETHERITE_INGOT);
+    });
+
     public static final RegistryObject<Item> BLACK_OPAL = ITEMS.register("black_opal",
             () -> new Item(new Item.Properties()));
 
@@ -20,7 +26,7 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> BLACK_OPAL_PICKAXE = ITEMS.register("black_opal_pickaxe",
-            () -> new PickaxeItem(Tiers.NETHERITE, 1,-2.8f, (new Item.Properties())));
+            () -> new PickaxeItem(BLACKOPAL, 1, -2.8f, (new Item.Properties())));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
